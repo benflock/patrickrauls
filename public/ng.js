c1602f3d6c1a412b2a04b26601a3388ce1eba2b1
Patrick Rauls <patrickrauls@Patricks-MacBook-Air.local>
2017-02-23 14:58:23 -0700

    files

---
+++ public/ng.js
@@ -0,0 +1,39 @@
+(function() {
+    angular.module('app', [])
+    .component('navMenu', {
+        controller() {
+            console.log('nav-menu')
+        },
+        templateUrl: './templates/nav-menu.html'
+    })
+    .component('landing', {
+        controller() {
+            console.log('landing')
+        },
+        templateUrl: './templates/landing.html'
+    })
+    .component('thoughts', {
+        controller() {
+            console.log('thoughts')
+        },
+        templateUrl: './templates/thoughts.html'
+    })
+    .component('projects', {
+        controller() {
+            console.log('projects')
+        },
+        templateUrl: './templates/projects.html'
+    })
+    .component('cv', {
+        controller() {
+            console.log('cv')
+        },
+        templateUrl: './templates/cv.html'
+    })
+    .component('holla', {
+        controller() {
+            console.log('holla')
+        },
+        templateUrl: './templates/holla.html'
+    })
+})();
