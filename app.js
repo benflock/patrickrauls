533c0688856abbe5dd033e16179be43613967c09
Patrick Rauls <patrickrauls@Patricks-MacBook-Air.local>
2017-02-23 14:28:40 -0700

    Nodemon

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 //modules
 const path = require('path'),
     express = require('express');
-
+//routes
+const routes = require('./routes/index.js');
 //derivatives
 const app = express();
 
